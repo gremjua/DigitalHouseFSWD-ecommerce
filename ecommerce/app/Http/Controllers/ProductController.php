89d0bafbdbfe9d04f58d3da13d26673aaeb41813
Juan Gremes <gremjua@gmail.com>
2019-12-17 22:37:39 -0300

    Updated product controller.

--- ecommerce/app/Http/Controllers/ProductController.php
+++ ecommerce/app/Http/Controllers/ProductController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Product;
 
 class ProductController extends Controller
 {
-    //
+    public function directory() {
+        $products = Product::orderBy('name')
+            ->paginate(9);
+        return view('products', compact('products'));
+    }
+
+    public function show($id) {
+        $product = Product::find($id);
+        return view('product', compact('product'));
+    }
 }
